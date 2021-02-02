2d6cfb0162502b7b39730f33b9f603ca1326e187
Prudhvi Naidu <55322101+prudhvinaidu9@users.noreply.github.com>
2021-02-03 00:19:51 +0530

    Update sort0s1s2s.cpp

--- Arrays/sort0s1s2s.cpp
+++ Arrays/sort0s1s2s.cpp
@@ -23,10 +23,10 @@ void sortcolors(vector<int> &v)
 
 int main() 
 { 
-	#ifndef ONLINE_JUDGE
-	freopen("input1.txt","r",stdin);
-	freopen("output1.txt","w",stdout);
-	#endif
+	//#ifndef ONLINE_JUDGE
+	//freopen("input1.txt","r",stdin);
+	//freopen("output1.txt","w",stdout);
+	//#endif
 	int n;
 	cin>>n;
 	vector<int>v;
