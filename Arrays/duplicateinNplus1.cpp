1b40bb84943931a1d14dddf112334828d5621024
Prudhvi Naidu <55322101+prudhvinaidu9@users.noreply.github.com>
2021-02-05 00:01:06 +0530

    Update duplicateinNplus1.cpp

--- Arrays/duplicateinNplus1.cpp
+++ Arrays/duplicateinNplus1.cpp
@@ -21,10 +21,6 @@ int duplicatenum(vector<int>&v)
 
 int main() 
 { 
-	// #ifndef ONLINE_JUDGE
-	// freopen("input1.txt","r",stdin);
-	// freopen("output1.txt","w",stdout);
-	// #endif
 	int n;
 	cin>>n;
 	vector<int>v;
